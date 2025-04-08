561f1f678ef176f1423d267a8409428299cdaf9f
kimown <kimown@163.com>
2025-04-08 14:42:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "cudaMain.h"
 
+
+void test(){
+    int aa=2;
+    int bb = aa+10;
+}
+
 int main(int argc, char **argv) {
+    int a=1;
+    int b=1+1;
+    test();
     return cudaMain(argc, argv);
 //    return 0;
 }
